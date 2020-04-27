fbdb91b5821d4b9a4fa5b70f9611f67e51449280
Ihor Husar <ihor.husar@gmail.com>
2020-04-26 21:23:05 -0400

    refactored code

--- js/main.js
+++ js/main.js
@@ -1,14 +1,56 @@
-var camera, scene, renderer;
+var camera, scene, renderer, rowN, spriteN
 var groups = [];
+
+var RATIO_PER_100 = 0.4, SPRITE_HEIGHT = 80, SPRITE_WIDTH = 80, SPRITE_SPACING = 40
+
+class Row {
+	static get DIRECTION_UP(){
+		return 1
+	}
+	static get DIRECTION_DOWN(){
+		return -1
+	}
+	CHANGE=1
+
+	constructor(dir, group){
+		this.direction = dir || Row.DIRECTION_UP
+		this.group = group
+	}
+
+	reverse(){
+		this.direction *= -1
+	}
+
+	update(){
+		this.group.position.x += this.direction * this.CHANGE
+		this.group.position.y += this.direction * this.CHANGE 
+		if(this.group.position.y > window.innerHeight + SPRITE_HEIGHT || this.group.position.y + (spriteN) * (SPRITE_HEIGHT + SPRITE_SPACING) < 0 ){
+			this.reverse()
+		}
+	}
+}
 
 init();
 animate();
 
 
+
+
+function getRowsN(width, height){
+	return Math.floor(RATIO_PER_100 * (width + height) / 100) + 1;
+}
+
+function getSpriteN(width, height){
+	return Math.floor(1.5 * (width + height) / (SPRITE_HEIGHT + SPRITE_SPACING)) + 1
+}
+
 function init() {
 
 	var width = window.innerWidth;
 	var height = window.innerHeight;
+
+	rowN = getRowsN(width, height);
+
 
 	camera = new THREE.OrthographicCamera( 0, width, height, 0, 1, 100 );
 	camera.position.z = 100;
@@ -19,8 +61,7 @@ function init() {
 
 	// create sprites
 
-	var amount = 30;
-	var radius = 50;
+	spriteN = getSpriteN(width, height);
 
 	var textureLoader = new THREE.TextureLoader();
 
@@ -29,19 +70,19 @@ function init() {
 	let sprite = new THREE.Sprite( spriteMaterial );
 
 	console.log(spriteMaterial)
-	let sprite_width = spriteMaterial.map.image || 256
-	let sprite_height = spriteMaterial.map.image || 256
+	let sprite_width = SPRITE_WIDTH
+	let sprite_height = SPRITE_HEIGHT
 	sprite.center.set(0.5, 0.5)
 	console.log(sprite_width, sprite_height)
 	sprite.scale.set(sprite_width, sprite_height, 1)
 	
-	for(let i = 0; i<15; i++){
+	for(let i = 0; i<rowN; i++){
 		let group = new THREE.Group();
 
-		for ( let a = 0; a < amount; a ++ ) {
+		for ( let a = 0; a < spriteN; a ++ ) {
 
-			var x = a * 150;
-			var y = a * 150;
+			var x = a * (SPRITE_HEIGHT + SPRITE_SPACING);
+			var y = a * (SPRITE_HEIGHT + SPRITE_SPACING);
 			var z = 0;
 
 			var material;
@@ -53,7 +94,7 @@ function init() {
 			} else {
 
 				material = spriteMaterial.clone();
-				material.color.setHSL( 0.5 * Math.random(), 0.75, 0.5 );
+				material.color.setHSL( 0.7*Math.random(), 0.7 * Math.random(), 0.7 * Math.random() );
 				material.map.offset.set( - 0.5, - 0.5 );
 				material.map.repeat.set( 2, 2 );
 
@@ -67,9 +108,20 @@ function init() {
 			group.add( sprite );
 
 		}
-		group.position.x = window.innerWidth - i * 300;
-		group.position.y = -500
-		groups.push(group);
+		group.position.x =  - height + i * Math.floor(100 / RATIO_PER_100);
+		group.position.y = 0
+		
+		var row	
+		if(i % 2 == 0){
+			group.position.x -= (spriteN - 1) * (SPRITE_HEIGHT + SPRITE_SPACING)
+			group.position.y -= (spriteN - 1) * (SPRITE_HEIGHT + SPRITE_SPACING)
+			row = new Row(Row.DIRECTION_UP, group)
+		} else {
+			group.position.x += height
+			group.position.y += height
+			row = new Row(Row.DIRECTION_DOWN, group)
+		}
+		groups.push(row);
 		scene.add( group );
 	}
 
@@ -95,11 +147,22 @@ function onWindowResize() {
 	var height = window.innerHeight;
 	camera.left = 0
     camera.right = width
-    camera.top = 0
-    camera.bottom = - height
+    camera.top = height
+    camera.bottom = 0
     camera.updateProjectionMatrix()
 	renderer.setSize( width,  height );
+	updateRows(width, height);
 }
+
+function updateRows(width, height){
+	let newN = getRowsN(width, height)
+	if( newN > rowN ){
+		for(let i=0; i<newN-rowN; i++){
+
+		}
+	}
+}
+
 
 function animate() {
 
@@ -114,12 +177,7 @@ function render() {
 
 	for (let i = 0; i<groups.length; i++){
 		let group = groups[i]
-		let change = -0.50
-		if (i%2==0){
-			change = 0.50
-		}
-		groups[i].position.x += change
-		groups[i].position.y += change
+		group.update()
 	}
 
 	renderer.render( scene, camera );
